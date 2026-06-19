445ac538582fe1b7c0368d75b46779b4ca693bcb
Prasad-555 <prasadchinchole555@gmail.com>
2026-06-19 11:12:13 +0530

    updated one server required

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,20 @@ import express from "express";
 import dotenv from "dotenv"
 import authRouter from "./routes/authRoute.js"
 import messageRoute from "./routes/messageRoute.js"
+import path from "path";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000; 
+const __dirname = path.resolve(); 
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRoute);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend")))
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
 
 app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
